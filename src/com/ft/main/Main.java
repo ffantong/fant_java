31fe1def15d012ef3032f2371a8a22f7f4638c7e
ffantong <875488404@qq.com>
2016-08-30 13:31:48 +0800

    update

--- src/com/ft/main/Main.java
+++ src/com/ft/main/Main.java
@@ -12,10 +12,6 @@ import com.ft.xmlparser.Parser;
  *
  */
 public class Main {
-	/**
-	 * 版本信息，小的改动增加0.0.1
-	 * 大的改动增加0.1  不断完善中
-	 */
 	private static final String version = "2.1.2";
 
 	public static void main(String[] args) {
